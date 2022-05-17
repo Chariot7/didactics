ead218fee151736dadc18dc12168d18c0059dcf8
Chariot7 <brennan.c.lowe@gmail.com>
2022-05-17 06:28:07 -0400

    heroku test

--- src/Components/SliderDataOne.js
+++ src/Components/SliderDataOne.js
@@ -1,11 +1,17 @@
 export const SliderData = [
     {
-        image: 'https://i.imgur.com/8vp3s5H.png'
+        image: 'https://i.imgur.com/0rlNlNF.png'
     },
     {
-        image: 'https://i.imgur.com/3dECdjh.png'
+        image: 'https://i.imgur.com/cjHcwEv.png'
     },
     {
-        image: 'https://i.imgur.com/3U0Q9eR.png'
+        image: 'https://i.imgur.com/z5jwMsx.png'
+    },
+    {
+        image: 'https://i.imgur.com/0SBzC0z.png'
+    },
+    {
+        image: 'https://i.imgur.com/v3ONkmE.jpg'
     },
 ]
