40846ba7b0590e5fcd26c72f9e7a8e388a2cca24
Chariot7 <brennan.c.lowe@gmail.com>
2022-05-17 11:04:22 -0400

    artworks

--- src/Components/StyledNavbar.js
+++ src/Components/StyledNavbar.js
@@ -7,16 +7,16 @@ export const Navbar = () => {
                 <div className='navlink'>
                     <ul>
                         <li>
-                            <NavLink to="/screen1">Screen 1</NavLink> 
+                            <NavLink to="/screen1">Artwork 1</NavLink> 
                         </li>
                         <li>
-                            <NavLink to="/screen2">Screen 2</NavLink> 
+                            <NavLink to="/screen2">Artwork 2</NavLink> 
                         </li>
                         <li>
-                            <NavLink to="/screen3">Screen 3</NavLink> 
+                            <NavLink to="/screen3">Artwork 3</NavLink> 
                         </li>
                         <li>
-                            <NavLink to="/screen4">Screen 4</NavLink> 
+                            <NavLink to="/screen4">Artwork 4</NavLink> 
                         </li>
                         <li>
                             <NavLink to="/artists">Artists</NavLink> 
