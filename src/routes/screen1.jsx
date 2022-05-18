e3243747c640621a082715a09b60f22521917e86
Chariot7 <brennan.c.lowe@gmail.com>
2022-05-18 09:30:27 -0400

    screen switch

--- src/routes/screen1.jsx
+++ src/routes/screen1.jsx
@@ -1,5 +1,5 @@
 import { ImageSlider } from "../Components/ImageSlider";
-import { SliderData } from "../Components/SliderDataOne";
+import { SliderData } from "../Components/SliderDataFour";
 
 export default function Screen1() {
   return (
@@ -7,47 +7,28 @@ export default function Screen1() {
       <ImageSlider slides={SliderData} />
       <div className="text">
         <body>
-          <div style={{
-            paddingTop: '15px'
+        <div style={{
+            paddingTop: '20px'
           }}>
           <h2>Jennifer and Kevin McCoy</h2>
           </div>
 
           <span className="title">
-              Quantum Leap: Primordial Star 3, 2022
+              Quantum Leap 3: Dark Star, 2022
           </span>
-          <p>
-            <span className="italic">Quantum Leap: Primordial Star 3</span> is a new NFT from the series <span className="italic">Quantum
-            Leap</span>, inspired by Kevin McCoy’s 2014 work <span className="italic">Quantum</span>, the first artwork
-            ever to be tokenized on a blockchain. The word “quantum” alludes to
-            the attempt in theoretical physics to develop a theory of the
-            universe and to answer questions about the first phases of the
-            universe. This generative, iterative process is emulated in the
-            structure of <span className="italic">Quantum Leap</span>, which evolves to become more complex,
-            changing states and spinning off new versions of itself over time. 
-          </p>
-          <p>
-            In <span className="italic">Quantum Leap: Primordial Star 3</span>, several levels of code-based
-            systems interact. Visually, each token from <span className="italic">Quantum Leap</span> produces a
-            mandala that changes and evolves, inspired by the life cycle of the
-            stars, which shift colors as they age. These “stars” also change
-            form as they become more circular as the code-generated images
-            mature, evolving from three to twenty sides over the span of their
-            lifetimes. 
-          </p>
           <p style={{
-            paddingBottom: '54px'
+            paddingBottom: '625px'
           }}>
-            The code-execution environment that drives the animation exists both
-            on a dedicated website, and eventually, as a downloadable standalone
-            application. This website allows collectors who have <span className="italic">Quantum Leap </span> 
-            tokens to visit the site, connect their wallets (or download the
-            application), and see the artwork that corresponds to their token.
-            The software will read the specific tokens and derive operational
-            parameters from on-chain data belonging to each token.
+            Each <span className="italic">Quantum Leap: Primordial Star</span> slowly loses its color over the
+            course of its lifespan, finally producing a Dark Star. This token is
+            a “memento mori,” an animated ghost of what was once living. After
+            producing this token, <span className="italic">Quantum Leap: Primordial Star</span> is reborn in
+            full color with a new central shape and palette, replicating and
+            generating new tokens autonomously.  
           </p>
           <hr></hr>
           <div className="product" style={{ display: "flex" }}>
+            
             <img
               className="productImg"
               src={
@@ -55,8 +36,13 @@ export default function Screen1() {
               }
               alt="artwork"
             />
-            <div className="productText">
-              <h3 className="productHeader">LG Transparent OLED</h3>
+            <div style={{
+              paddingTop: '23px',
+              paddingLeft: '17px'
+            }}>
+              <h3 className="productHeader">
+              LG Transparent OLED
+              </h3>
               <p className="productSpecs">
                 55 inch, 1920x1080, FHD <br></br>
                 Anti-Reflection treatment on 4mm safety glass <br></br>
--- src/routes/screen4.jsx
+++ src/routes/screen4.jsx
@@ -1,5 +1,5 @@
 import { ImageSlider } from "../Components/ImageSlider";
-import { SliderData } from "../Components/SliderDataFour";
+import { SliderData } from "../Components/SliderDataOne";
 
 export default function Screen4() {
   return (
@@ -7,28 +7,48 @@ export default function Screen4() {
       <ImageSlider slides={SliderData} />
       <div className="text">
         <body>
-        <div style={{
-            paddingTop: '20px'
+          <div style={{
+            paddingTop: '15px'
           }}>
           <h2>Jennifer and Kevin McCoy</h2>
           </div>
 
           <span className="title">
-              Quantum Leap 3: Dark Star, 2022
+              Quantum Leap: Primordial Star 3, 2022
           </span>
+          <p>
+            <span className="italic">Quantum Leap: Primordial Star 3</span> is a new NFT from the series <span className="italic">Quantum
+            Leap</span>, inspired by Kevin McCoy’s 2014 work <span className="italic">Quantum</span>, the first artwork
+            ever to be tokenized on a blockchain. The word “quantum” alludes to
+            the attempt in theoretical physics to develop a theory of the
+            universe and to answer questions about the first phases of the
+            universe. This generative, iterative process is emulated in the
+            structure of <span className="italic">Quantum Leap</span>, which evolves to become more complex,
+            changing states and spinning off new versions of itself over time. 
+          </p>
+          <p>
+            In <span className="italic">Quantum Leap: Primordial Star 3</span>, several levels of code-based
+            systems interact. Visually, each token from <span className="italic">Quantum Leap</span> produces a
+            mandala that changes and evolves, inspired by the life cycle of the
+            stars, which shift colors as they age. These “stars” also change
+            form as they become more circular as the code-generated images
+            mature, evolving from three to twenty sides over the span of their
+            lifetimes. 
+          </p>
           <p style={{
-            paddingBottom: '625px'
+            paddingBottom: '54px'
           }}>
-            Each <span className="italic">Quantum Leap: Primordial Star</span> slowly loses its color over the
-            course of its lifespan, finally producing a Dark Star. This token is
-            a “memento mori,” an animated ghost of what was once living. After
-            producing this token, <span className="italic">Quantum Leap: Primordial Star</span> is reborn in
-            full color with a new central shape and palette, replicating and
-            generating new tokens autonomously.  
+            The code-execution environment that drives the animation exists both
+            on a dedicated website, and eventually, as a downloadable standalone
+            application. This website allows collectors who have <span className="italic">Quantum Leap </span> 
+            tokens to visit the site, connect their wallets (or download the
+            application), and see the artwork that corresponds to their token.
+            The software will read the specific tokens and derive operational
+            parameters from on-chain data belonging to each token.
           </p>
           <hr></hr>
           <div className="product" style={{ display: "flex" }}>
-            <img
+          <img
               style={{
                 width: '39%'
               }}
@@ -37,15 +57,11 @@ export default function Screen4() {
               }
               alt="artwork"
             />
-            <div style={{
-              paddingTop: '23px',
-              paddingLeft: '17px'
-            }}>
+            <div className="productText">
               <h3 className="productHeader">
-                LG 65-inch OLED evo Objet Collection
-              </h3>
+              LG 65-inch OLED evo Objet Collection</h3>
               <p className="productSpecs">
-                a9 Gen4 AI Processor <br></br>
+              a9 Gen4 AI Processor <br></br>
                 OLED with Self-Lit Pixels <br></br>
                 Ultra HD (3,840 x 2,160) <br></br>
                 80 W Speaker Output
